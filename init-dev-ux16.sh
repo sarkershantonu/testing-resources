364cbff3d75612a14ffa247a58e9686c8d7922fd
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2017-04-14 16:22:49 -0400

    Update init-dev-ux16.sh

--- init-dev-ux16.sh
+++ init-dev-ux16.sh
@@ -1,1 +1,26 @@
 
+#!/bin/sh
+
+#install Git
+
+#install SmartGit
+
+#install CURL
+
+#Install oracle Java 8
+
+#install Maven
+
+#download/Install intellij Idea 
+
+#Download/Install Atom
+
+#Download/Install VS Code
+
+#Jmeter Installation 
+
+#Selenium installation 
+
+#Setup Code Repository
+
+#Setup Java Tool repository
